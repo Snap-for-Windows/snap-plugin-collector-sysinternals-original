d6a9c6afc9cfda98e8cbc1bf7443243fd53204af
Phillip Anderson <phill.eanderson@gmail.com>
2016-11-26 21:54:20 -0700

    Code added to create text file and remove unneeded info from header

--- raw.go
+++ raw.go
@@ -1,29 +1,56 @@
 package main
 
 import "os/exec"
+
+import "os"
+import "io"
+import "fmt"
 import "strings"
+import "strconv"
+import "bytes"
 
 func main() {
 	// run(exec.Command("cmd", "-Command", "netstat"))
 	cmd := exec.Command("pslist")
 	stdout, err := cmd.Output()
-
+	fmt.Println("cmon!")
 	if err != nil {
 		println(err.Error())
 		return
 	}
 	birdistheword := string(stdout)
-	//print(birdistheword)
+	// print(birdistheword)
 
-	row := strings.Split(birdistheword, "\n")
+	row := strings.Fields(birdistheword)
 	print(len(row))
 
-	for index, element := range row {
-		print("jhfghljhgyftrdfdfcvjjliuytfrdgfcvhbjiuiyt")
-		print(element)
-		print(index)
+	var stringSlice []string
+	stringSlice = strings.Split(birdistheword, "\n")
+	fmt.Println(strconv.Itoa(len(stringSlice)) + " what up")
+	// i := 6
+	stringSlice = append(stringSlice[:0], stringSlice[8:]...)
+	fmt.Println(strconv.Itoa(len(stringSlice)) + " the second")
 
+	// for index, element := range row {
+	// 	print(element)
+	// 	print(index)
+	// }
+	var buffer bytes.Buffer
+	for _, theList := range stringSlice {
+		buffer.WriteString(theList)
 	}
+	print(buffer.String())
+	filename := "output.txt"
+	fmt.Println("writing: " + filename)
+	fo, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	n, err := io.WriteString(fo, buffer.String())
+	if err != nil {
+		fmt.Println(n, err)
+	}
+	fo.Close()
 
 	//http://stackoverflow.com/questions/6182369/exec-a-shell-command-in-go
 }
