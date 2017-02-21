fd696d77a4f0a94ad908a7c7500be550e9cba68a
Phillip Anderson <phill.eanderson@gmail.com>
2017-02-20 18:16:12 -0700

    Update to include proper folder for sysinternals

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	// Import the more recent gRPC (Go RPC plugin lib is now deprecated) snap plugin library
 	"github.com/intelsdi-x/snap-plugin-lib-go/v1/plugin" 
 	// Import our collector plugin implementation
-	"github.com/Snap-for-Windows/snap-plugin-collector-sysinternals"
+	"github.com/Snap-for-Windows/snap-plugin-collector-sysinternals/sysinternals"
 )
 
 const (
